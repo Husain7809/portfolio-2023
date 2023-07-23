4be71a2c66adbec4ff0960d0e07af3bc2a8ac20e
Husain <varaliyamahammad86@gmail.com>
2023-07-23 17:38:18 +0530

    changes images path

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -22,7 +22,7 @@ const Project = ({ setProgress }) => {
               <div className='border-4 border-gray-500 shadow-lg project_section'>
                 {item.image ?
                   <div className="image">
-                    <img src={`../../public/assets/${item.image}`} alt="project_image" />
+                    <img src={`./${item.image}`} alt="project_image" />
                   </div>
                   : ""
                 }
