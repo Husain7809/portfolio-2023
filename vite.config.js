de7e96f0cb441b0f3229d29ed7c393fdcf13ede7
Husain <varaliyamahammad86@gmail.com>
2023-07-23 18:01:56 +0530

    Change config file

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  plugins: [react()],
+  root: './',
   build: {
     outDir: 'dist',
   },
-  plugins: [react()],
-  server: {
-    ssr: false,
-  },
-});
+})
