50df961d81040c913958f70e6e4953886e76e98e
Husain <varaliyamahammad86@gmail.com>
2023-08-18 22:27:48 +0530

    profile icon href change

--- src/pages/NavBar.jsx
+++ src/pages/NavBar.jsx
@@ -5,7 +5,7 @@ const NavBar = () => {
     <>
       <nav className="flex flex-col w-56 text-white ">
         <div className="flex items-center justify-center">
-          <a className="rounded-full profile" href="/">
+          <a className="rounded-full profile" href="#">
             {/* <img src='../../public/images/cartoon.png' alt="logo" /> */}
             <img src='https://cdn.hashnode.com/res/hashnode/image/upload/v1612774146044/LOmtsiohU.jpeg?w=256&h=256&fit=crop&crop=entropy&auto=compress,format&format=webp' alt="logo" />
           </a>
